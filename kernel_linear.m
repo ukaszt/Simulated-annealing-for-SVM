b1e4b3d45a03c83663cf185e2d9dcba350ec50d9
Piotr D <pejotr.pl@gmail.com>
2012-01-15 00:27:44 +0100

    Parametrized linear kernel function

---
+++ kernel_linear.m
@@ -0,0 +1,6 @@
+function x = kernel_linear(u, v, p1)
+    % The Linear kernel is the simplest kernel function. It is given by the
+    % inner product <x,y> plus an optional constant p1.
+    x = u*v' + p1;  
+end
+
