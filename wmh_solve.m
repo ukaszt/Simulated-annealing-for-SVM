2418ad9eaef0df7ca5f233f082a0239d086d3e3c
ukaszt <ukaszt@gmail.com>
2012-01-15 01:58:24 +0100

    wmh_solve init add

--- wmh_solve.m
+++ wmh_solve.m
@@ -1,5 +1,9 @@
 function wmh_solve()
     %anonymous fun for passing arguments
     %@(u,v) kfun(u,v,p1,p2)
-
+    train = dlmread('train.txt');
+    test  = dlmread('test.txt');
     
+    p0 = [1 1];
+    min_fun = @(p) svmsa(train, test,  @(u, v) kernel_tanh(u, v, p(1), p(2)));
+    [x, fval, exitFlag, output] = simulaatedannealbnd(min_fun, p0);
