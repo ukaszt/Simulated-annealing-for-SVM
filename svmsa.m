fba32dbf9123d927f4ee32356b2d68e48b943754
ukaszt <ukaszt@gmail.com>
2012-01-15 23:29:29 +0100

    multiclass svmsa.m

--- svmsa.m
+++ svmsa.m
@@ -1,7 +1,25 @@
 function [ errorn ] = svmsa(trnd, trng, tstd, tstg, kernel)
-
-    svm_classfier = svmtrain(trnd, trng, 'kernel_function', kernel, 'autoscale', false);
-    svm_classify_result = svmclassify(svm_classfier, tstd);
+    T=trnd
+    C=tstg
+    u=unique(C);
+    N=length(u);
+    if(N>2)
+        disp('multi class problem');
+        itr=1;
+        classes=0;
+        while((classes~=1)&&(itr<=length(u)))
+            c1=(C==u(itr));
+            newClass=c1;
+            svm_classfier = svmtrain(T,newClass, 'kernel_function', kernel, 'showplot', true, 'autoscale', false);
+            svm_classify_result = svmclassify(svm_classfier,tstd,'showplot',true);
+            itr=itr+1;
+        end
+    %clc;
+    itr=itr-1;
+    disp(itr)
+    end
     
     errorn = sum(~(svm_classify_result == tstg));
+    disp('NUM ERR:')
+    disp(errorn)
 end
