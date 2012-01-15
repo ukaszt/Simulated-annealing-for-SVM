a0726c8a69a59a94440d636ea3ce8985d1f27356
Piotr D <pejotr.pl@gmail.com>
2012-01-15 21:57:36 +0100

    donio development

--- svmsa.m
+++ svmsa.m
@@ -1,19 +1,7 @@
-function [ errorn ] = svmsa(train, test, kernel)
-%SVM-SA Summary of this function goes here
-%   Detailed explanation goes here
-    
-    [r1, c1] = size(train);
-    [r2, c2] = size(test);
+function [ errorn ] = svmsa(trnd, trng, tstd, tstg, kernel)
 
-    train_data = train(1:r1, 1:c1-1);
-    train_grps = train(1:r1, c1);
+    svm_classfier = svmtrain(trnd, trng, 'kernel_function', kernel, 'autoscale', false);
+    svm_classify_result = svmclassify(svm_classfier, tstd);
     
-    test_data = test(1:r2, 1:c2-1);
-    test_grps = test(1:r2, c2);     
-    
-    svm_classfier = svmtrain(train_data, train_grps, 'kernel_function', kernel, 'showplot', true);
-    hold on
-    svm_classify_result = svmclassify(svm_classfier, test_data, 'showplot', true);
-
-    errorn = sum(~(svm_classify_result == test_grps));
+    errorn = sum(~(svm_classify_result == tstg));
 end
--- wmh_solve.m
+++ wmh_solve.m
@@ -1,9 +1,36 @@
-function wmh_solve()
-    %anonymous fun for passing arguments
-    %@(u,v) kfun(u,v,p1,p2)
-    train = dlmread('train.txt');
-    test  = dlmread('test.txt');
+function wmh_solve(kernel_type)
+
+    %trn = dlmread('data/rs_train5_no_headers_mini.txt');
+    %tst = dlmread('data/rs_test5_no_headers_mini.txt');
     
-    p0 = [1 1];
-    min_fun = @(p) svmsa(train, test,  @(u, v) kernel_tanh(u, v, p(1), p(2)));
-    [x, fval, exitFlag, output] = simulaatedannealbnd(min_fun, p0);
+    trn = dlmread('data/devtrain.txt');
+    tst = dlmread('data/devtest.txt');
+    
+    [trn_r, trn_c] = size(trn);
+    [tst_r, tst_c] = size(tst);
+  
+    switch kernel_type
+        case 'hiperbolic'
+            p0 = [1 1];
+            min_fun = @(p) svmsa(trn(:, 1:trn_c - 1), trn(:, trn_c), ...
+                tst(:, 1:tst_c - 1), tst(:, tst_c),  ...
+                @(u, v) kernel_tanh(u, v, p(1), p(2)));     
+        case 'polynomial'
+            p0 = [-3 -3];
+            min_fun = @(p) svmsa(trn(:, 1:trn_c - 1), trn(:, trn_c), ...
+                tst(:, 1:tst_c - 1), tst(:, tst_c),  ...
+                @(u, v) kernel_polynomial(u, v, p(1), p(2), 3));   
+    end
+    
+    options = saoptimset('MaxIter', 1500);
+    [x fval] = simulannealbnd(min_fun, p0, [0 -2], [10 10], options);
+    
+    x
+    
+    hold on
+    svm_classfier = svmtrain(trn(:, 1:(trn_c - 1)), trn(:, trn_c), ...
+        'kernel_function', @(u, v) kernel_polynomial(u, v, x(1), x(2), 3), ...
+        'autoscale', false, 'showplot', true);
+    svmclassify(svm_classfier, tst(:, 1:tst_c - 1), 'showplot', true);
+    
+    
