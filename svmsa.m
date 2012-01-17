d82d5a10e10791f516ed69b1091a8247a650fce4
Piotr D <pejotr.pl@gmail.com>
2012-01-17 02:14:38 +0100

    multiclass svm - one versus one

--- svmsa.m
+++ svmsa.m
@@ -1,25 +1,39 @@
 function [ errorn ] = svmsa(trnd, trng, tstd, tstg, kernel)
-    T=trnd
-    C=tstg
-    u=unique(C);
-    N=length(u);
-    if(N>2)
-        disp('multi class problem');
+    unqcls = unique([trng; tstg]);
+    unqclsno = length(unqcls);
+    
+    trn    = [trnd trng];
+    tstno  = size(tstd);
+    newcls = zeros(tstno(2), unqclsno);
+    
+    % Traning classifiers
+    if(unqclsno > 2)
+        prs = nchoosek(unqcls, 2);
         itr=1;
-        classes=0;
-        while((classes~=1)&&(itr<=length(u)))
-            c1=(C==u(itr));
-            newClass=c1;
-            svm_classfier = svmtrain(T,newClass, 'kernel_function', kernel, 'showplot', true, 'autoscale', false);
-            svm_classify_result = svmclassify(svm_classfier,tstd,'showplot',true);
+        while(itr <= length(prs))
+            clsa = trn(find(trn(:, end) == prs(itr, 1)), :); % wiersze danych trenujacych spelniajace warunke numeru klasy
+            clsb = trn(find(trn(:, end) == prs(itr, 2)), :); % wiersze danych trenujacych spelniajace warunke numeru klasy
+            svmclsfr = svmtrain([ clsa(:, 1:end-1); clsb(:, 1:end-1)], ...
+                [clsa(:, end); clsb(:, end)], 'kernel_function', kernel, ... 
+                'showplot', false, 'autoscale', false);
+            
+            itr2=1;
+            while itr2 <= tstno(1)
+                res = svmclassify(svmclsfr, tstd(itr2, :));
+                newcls(itr2, find( unqcls == res)) = newcls(itr2, find( unqcls == res )) + 1;
+                itr2=itr2 + 1;
+            end            
             itr=itr+1;
         end
-    %clc;
-    itr=itr-1;
-    disp(itr)
     end
     
-    errorn = sum(~(svm_classify_result == tstg));
-    disp('NUM ERR:')
-    disp(errorn)
+    newcls
+    
+    % errorn = sum(~(svm_classify_result == tstg));
+    % disp('NUM ERR:')
+    % disp(errorn)
 end
+
+% a = magic(3)
+% a(1, 3) = 2
+% a(find( a(:,end) == 2 ), :)
