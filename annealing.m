441780fd2104f663da156a845d98ac8a0bd4235f
Piotr D <pejotr.pl@gmail.com>
2012-01-28 17:00:39 +0100

    small development for flexibility

--- annealing.m
+++ annealing.m
@@ -1,4 +1,4 @@
-function wmh_solve(trn, tst, kernel_type, time, saopt)
+function annealing(trn, tst, kernel_type, time, saopt)
 
     %trn = dlmread('data/rs_train7_no_headers.txt');
     %tst = dlmread('data/rs_test7_no_headers.txt');
@@ -7,8 +7,8 @@ function wmh_solve(trn, tst, kernel_type, time, saopt)
     %tst = dlmread('data/rs_test7_short.csv', ',');
     %trn = dlmread('data/rs_train7_short.csv', ',');
     
-    trn = dlmread('data/mltclass_train.txt');
-    tst = dlmread('data/mltclass_test.txt');
+    %trn = dlmread('data/mltclass_train.txt');
+    %tst = dlmread('data/mltclass_test.txt');
     
     [trn_r, trn_c] = size(trn);
     [tst_r, tst_c] = size(tst);
@@ -27,7 +27,7 @@ function wmh_solve(trn, tst, kernel_type, time, saopt)
         case 'rbf'
             p0 = [1];
             min_fun = @(p) svmsa(trn(:, 1:end - 1), trn(:, end), ...
-                tst(:, 1:end - 1), tst(:, end), @(u, v) kernel_rbf(u, v, p(1)));
+                tst(:, 1:end - 1), tst(:, end), 'rbf', p(1));
         case 'tanh'
             p0 = [0 0];
             min_fun = @(p) svmsa(trn(:, 1:trn_c - 1), trn(:, trn_c), ...
@@ -41,6 +41,7 @@ function wmh_solve(trn, tst, kernel_type, time, saopt)
     x
     fval
     output
+    output.temperature
     
     %hold on
     %svm_classfier = svmtrain(trn(:, 1:(trn_c - 1)), trn(:, trn_c), ...
